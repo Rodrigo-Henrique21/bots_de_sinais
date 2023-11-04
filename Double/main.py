f48ab85138faca58f393b198759b4cff502773fe
Rodrigo <95323435+Rodrigo-Henrique21@users.noreply.github.com>
2023-11-04 02:27:15 -0300

    Update main.py

--- Double/main.py
+++ Double/main.py
@@ -1,62 +1,31 @@
-import numpy as np
-import requests as rq
-import datetime as dt
-import pytz
-import time
+# importações
 
-status = 0
-cont = 0
-operacao_atual = 0
-hora_5m = 0
+from bot import *
+
+# instancias
+
+if __name__ == "__main__":
+
+    obj_double = double(cont,acertos,perdas,status)
+
+
+# codigo
 
 while True:
 
-    print(f'Rodada {cont} e status {status}')
-    print(f'varivel hora operacao atual {operacao_atual} e variavel 5m {hora_5m}')
-
-    dt_inicio = (dt.datetime.now(pytz.timezone('Europe/London')) - dt.timedelta(seconds=180)).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
-    dt_fim = dt.datetime.now(pytz.timezone('Europe/London')).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
-
-    url = f'https://blaze-4.com/api/roulette_games/history?startDate={dt_inicio}Z&endDate={dt_fim}Z&page=1'
-
-    chamada = rq.get(url)
-    retorno = chamada.json()
-
-    dados_json = retorno['records']
-
-    lista_historico = list(map(lambda x:x['color'],dados_json))
-    data_criacao = list(map(lambda x:x['created_at'],dados_json))
-    operacao_atual = (dt.datetime.strptime(data_criacao[0][:-1],"%Y-%m-%dT%H:%M:%S.%f")).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-7]
-
-    if status == 0:
-
-        hora_5m = (dt.datetime.strptime(data_criacao[0][:-1],"%Y-%m-%dT%H:%M:%S.%f") + dt.timedelta(seconds=300)).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-7]
-        status = 1
-        cont += 1
-
-
-    elif status == 1 and hora_5m == operacao_atual:
-        cont += 1
-        if lista_historico[0] == 'red':
-            for i in range(1,5):
-                if lista_historico[i] == 'red':
-                    print('Aguarde tem muitos vermelhos')
-                    status = 0
-                else:
-                    print('Aposte preto')
-                    status = 0
-        elif lista_historico[0] == 'black':
-            for i in range(1,5):
-                if lista_historico[i] == 'black':
-                    print('Aguarde tem muitos pretos')
-                    status = 0
-                else:
-                    print('Aposte vermelho')
-                    status = 0
-        else:
-            print('Aguarde, analisando')
-            status = 0
+    if (dt.datetime.now()).strftime('%H') == hora_fechamento:
+        break
     else:
-        cont += 1
-        pass
+        obj_double.cont += 1
+        dt_inicio,dt_fim = obj_double.definicao_criterios()
+        lista_historico,data_criacao = obj_double.requisicao_api(dt_inicio,dt_fim)
+        operacao_atual = obj_double.ultima_operacao(data_criacao)
 
+        print(f'rodada:{obj_double.cont}, rodada atual: {operacao_atual}, rodada alvo: {hora_5m}')
+
+        if obj_double.status == 0:
+            hora_5m,status = obj_double.verifica_tempo(data_criacao)
+
+        elif obj_double.status == 1 and hora_5m == operacao_atual:
+            status,sinal_enviado,lista_historico = obj_double.estrategia_inverso(lista_historico,dt_inicio,dt_fim)
+            obj_double.estrategia_gale(sinal_enviado,lista_historico,dt_inicio,dt_fim)
